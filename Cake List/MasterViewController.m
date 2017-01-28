02f789656497088f4144eb617db7ba7fd02d65ea
jonathan thornburg <reliable2192@gmail.com>
2017-01-28 12:41:26 -0600

    initial commit

---
+++ Cake List/MasterViewController.m
@@ -0,0 +1,83 @@
+//
+//  MasterViewController.m
+//  Cake List
+//
+//  Created by Stewart Hart on 19/05/2015.
+//  Copyright (c) 2015 Stewart Hart. All rights reserved.
+//
+
+#import "MasterViewController.h"
+#import "CakeCell.h"
+
+@interface MasterViewController ()
+@property (strong, nonatomic) NSArray *objects;
+@property (strong, nonatomic) NSString *urlString;
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _urlString = @"https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json";
+    [self getData];
+}
+
+#pragma mark - Table View
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.objects.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    CakeCell *cell = (CakeCell*)[tableView dequeueReusableCellWithIdentifier:@"CakeCell"];
+    
+    NSDictionary *object = self.objects[indexPath.row];
+    cell.titleLabel.text = object[@"title"];
+    cell.descriptionLabel.text = object[@"desc"];
+ 
+    
+    NSURL *aURL = [NSURL URLWithString:object[@"image"]];
+    NSData *data = [NSData dataWithContentsOfURL:aURL];
+    UIImage *image = [UIImage imageWithData:data];
+    [cell.cakeImageView setImage:image];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+// Functions that do heavy lifting should be done off the main thread.
+- (void)getData{
+    
+    NSURL *url = [NSURL URLWithString:@"https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json"];
+    
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    
+    NSError *jsonError;
+    id responseData = [NSJSONSerialization
+                       JSONObjectWithData:data
+                       options:kNilOptions
+                       error:&jsonError];
+    if (!jsonError){
+        self.objects = responseData;
+        [self.tableView reloadData];
+    } else {
+    }
+    
+}
+
+- (void)getDataAsynchronously:(NSString *)urlString completion:(void (^)(NSArray *))completion {
+    NSURL *url = [NSURL URLWithString:self.urlString];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+    }];
+    
+}
+
+@end
